5c70ca1c49ee5aa43b9e4436a9cffe8d7f051272
ankit <rocky.singh.0501@gmail.com>
2020-03-18 00:47:57 +0530

    My first store commit

---
+++ store/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.catalog, name='catalog'),
+    path('cart/',views.cart,name="cart"),
+    path('cart/remove/',views.removefromcart,name="remove"),
+    path('cart/cart/checkout/',views.checkout,name="checkout"),
+    path('cart/checkout/',views.checkout,name="checkout"),
+    path('cart/cart/checkout/complete/',views.completeOrder,name="complete_order"),
+    path('cart/checkout/complete/',views.completeOrder,name="complete_order"),
+    path('admin-login/',views.adminLogin,name="admin_login"),
+    path('admin-panel/',views.adminDashboard,name="admin"),
+]
